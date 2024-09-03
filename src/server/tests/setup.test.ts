1e6bb78ed2599e2411f41609f45a1fa20cf13d83
Florian Breuer <florian.breuer@medium.ag>
2024-09-03 09:44:35 +0200

    update tests

--- src/server/tests/setup.test.ts
+++ src/server/tests/setup.test.ts
@@ -1,16 +1,16 @@
 import { type inferProcedureInput } from "@trpc/server";
 import { expect, test } from "vitest";
 
-import { appRouter, type AppRouter } from "~/server/presentation/api/root";
+import { createCaller, type AppRouter } from "~/server/presentation/api/root";
 import { db } from "../infrastructure/db/drizzle";
 
 test("example router", async () => {
   const ctx = {
     headers: new Headers(),
     db: db,
-    currentUser: null
+    currentUser: null,
   };
-  const caller = appRouter.createCaller(ctx);
+  const caller = createCaller(ctx);
   type Input = inferProcedureInput<AppRouter["post"]["hello"]>;
   const input: Input = {
     text: "test",
