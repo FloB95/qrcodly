f57677566cc6a34d5e5bce9dba0926280116aeb5
Florian Breuer <florian.breuer@medium.ag>
2024-11-28 07:56:17 +0100

    build dynamic swagger ui import

--- src/components/doc/Swagger.tsx
+++ src/components/doc/Swagger.tsx
@@ -1,20 +1,17 @@
 "use client";
 import React, { useState } from "react";
-import SwaggerUI from "swagger-ui-react";
+import dynamic from "next/dynamic";
 import { Loader } from "../ui/loader";
+
+// Dynamischer Import von SwaggerUI mit ssr deaktiviert
+const SwaggerUI = dynamic(() => import("swagger-ui-react"), { ssr: false });
 
 export const Swagger = ({ swagger }: { swagger: object }) => {
   const [loading, setLoading] = useState(true);
 
   return (
     <>
-      <SwaggerUI
-        spec={swagger}
-        onComplete={(system) => {
-          console.log(system);
-          setLoading(false);
-        }}
-      />
+      <SwaggerUI spec={swagger} onComplete={() => setLoading(false)} />
       {loading && (
         <div className="my-32 flex justify-center">
           <Loader />
