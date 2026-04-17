b844de137ff14aa34d2b5119c9d24a1741215cbe
Florian Breuer <me@fb-dev.de>
2026-04-17 09:26:15 +0200

    optimize indesign ratelimit

--- apps/backend/src/core/rate-limit/rate-limit.policy.ts
+++ apps/backend/src/core/rate-limit/rate-limit.policy.ts
@@ -66,8 +66,8 @@ export const RATE_LIMIT_POLICIES: RateLimitPolicies = {
 	},
 	[RateLimitPolicy.QR_RENDER]: {
 		[RateLimitTier.ANONYMOUS]: 0,
-		[RateLimitTier.AUTHENTICATED]: 50,
-		[RateLimitTier.PRO_PLAN]: 200,
+		[RateLimitTier.AUTHENTICATED]: 200,
+		[RateLimitTier.PRO_PLAN]: 400,
 	},
 	[RateLimitPolicy.SURVEY_SUBMIT]: {
 		[RateLimitTier.ANONYMOUS]: 0,
